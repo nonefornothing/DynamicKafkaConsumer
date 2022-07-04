a6f1073830a940c472803a49c0a49052cbf12846
C120121003 <johan.prianto@bankmandiri.co.id>
2022-07-04 13:16:16 +0700

    CORS Config

--- src/main/java/com/bankmandiri/streamfailedpe/StreamFailedPeApplication.java
+++ src/main/java/com/bankmandiri/streamfailedpe/StreamFailedPeApplication.java
@@ -2,6 +2,9 @@ package com.bankmandiri.streamfailedpe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class StreamFailedPeApplication {
@@ -9,5 +12,15 @@ public class StreamFailedPeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StreamFailedPeApplication.class, args);
 	}
+
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins("*");
+//			}
+//		};
+//	}
 
 }
